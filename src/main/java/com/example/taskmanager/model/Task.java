dc3e0298e10441462c231c7e4511a32fe623599f
Fatih Ali <fatihali@stu.aydin.edu.tr>
2023-08-29 00:12:48 +0300

    project extended

--- src/main/java/com/example/taskmanager/model/Task.java
+++ src/main/java/com/example/taskmanager/model/Task.java
@@ -1,2 +1,20 @@
-package com.example.taskmanager.model;public class Task {
+package com.example.taskmanager.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private long id;
+
+    public String name;
+
+    public String category;
+
+    public boolean completion;
+
 }
